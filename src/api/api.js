3d07c579b0688452ff3f4db00d3a50344b90158a
eliotpp1 <eliot.pouplier@gmail.com>
2024-12-18 13:29:22 +0100

    fix delete deck

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://easydeck.alwaysdata.net/api",
   withCredentials: true, // Important pour envoyer des cookies ou des informations d'identification
 });
 //https://easydeck.alwaysdata.net/api
